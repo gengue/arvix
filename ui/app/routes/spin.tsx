51462db617f8c9dba78636130d702de668c44063
Genesis Guerrero <genesisdaft@gmail.com>
2024-12-24 17:40:20 +0100

    fix spin in all motions

--- ui/app/routes/spin.tsx
+++ ui/app/routes/spin.tsx
@@ -1,62 +1,55 @@
 import { Button } from "@nextui-org/button";
-import { ArrowLeft, ArrowRight, Video } from "lucide-react";
-import { useRouteLoaderData } from "react-router";
-import type { Route } from "./+types/spin";
-import { useEffect, useMemo, useState, useTransition } from "react";
+import { ArrowLeft, ArrowRight } from "lucide-react";
+import { useEffect, useReducer, useTransition } from "react";
 import React from "react";
-import type { LoaderData } from "./masterplan-layout";
+import { useRouteLoaderData } from "react-router";
 import type { SpinRecord } from "~/lib/pb.types";
+import type { Route } from "./+types/spin";
+import type { LoaderData } from "./masterplan-layout";
 
-type StepStatus = {
-	idx: number;
-	motion: "intro" | "forward" | "back";
-};
-
-//TODO: fix changing motions
 //TODO: rename backVideo to backwardVideo
+//TODO: disable button where is playing
 export default function SpinPage({ loaderData, params }: Route.ComponentProps) {
 	const data = useRouteLoaderData<LoaderData>("routes/masterplan-layout");
-	const [step, setStep] = useState<StepStatus>({
-		idx: Number.parseInt(params.step || "0"),
-		motion: !params.step ? "intro" : "forward",
+	const [state, dispatch] = useReducer(reducer, {
+		type: "intro",
+		transitionVideo: data?.intro?.forwardVideo || "",
+		intro: data?.intro,
+		all: arrayToObject(data?.spin || []),
+		spinId: 0,
 	});
 	const videoRef = React.useRef<HTMLVideoElement>(null);
-
-	const view = useMemo(() => {
-		if (step.motion === "intro") {
-			return {
-				...data?.intro,
-				currentVideo: data?.intro?.forwardVideo,
-			};
-		}
-		return {
-			...data?.spin[step.idx],
-			currentVideo: step.motion === "back" ? data?.spin[step.idx]?.backVideo : data?.spin[step.idx]?.forwardVideo,
-		};
-	}, [data?.intro, data?.spin, step]);
 
 	const [isPending, startTransition] = useTransition();
 	usePreloadSpingVideos(data?.spin);
 
-	if (!data?.spin) return null;
+	const isPlaying = !!(
+		(videoRef?.current?.currentTime ?? 0) > 0 &&
+		!videoRef?.current?.paused &&
+		!videoRef?.current?.ended
+	);
+
+	console.log("isPlaying", isPlaying);
+	console.log("videoRef", videoRef?.current);
+
+	useEffect(() => {
+		if (videoRef.current) {
+			videoRef.current.src = state.transitionVideo;
+			videoRef.current.load();
+			if (isPlaying) {
+				videoRef.current.play();
+			}
+			videoRef.current.play();
+		}
+	}, [state.transitionVideo, isPlaying]);
+
+	if (!state?.transitionVideo) return null;
 
 	const handleSpinStep = (motion: "forward" | "back") => {
 		startTransition(() => {
-			setStep((prev) => ({
-				idx: (prev.idx + (motion === "forward" ? 1 : -1) + data.spin.length) % data.spin.length,
-				motion,
-			}));
-			if (videoRef.current) {
-				const newSrc = view[`${motion}Video`];
-				if (!newSrc || !videoRef.current) return;
-				videoRef.current.src = newSrc;
-				videoRef.current.play();
-			}
+			dispatch({ type: motion });
 		});
 	};
-
-	console.log("step", step, "view", view);
-	// console.log("view", view);
 
 	return (
 		<main>
@@ -69,12 +62,12 @@ export default function SpinPage({ loaderData, params }: Route.ComponentProps) {
 				preload="auto"
 				// style={{ opacity: 0 }}
 				onEnded={() => {
-					if (step.motion === "intro") {
-						setStep({ idx: 0, motion: "forward" });
+					if (state.type === "intro") {
+						dispatch({ type: "forward" });
 					}
 				}}
 			>
-				<source src={view.currentVideo} type="video/mp4" />
+				<source type="video/mp4" />
 				Your browser does not support the video tag.
 			</video>
 			<div className="absolute-center w-full flex justify-between z-20">
@@ -85,7 +78,7 @@ export default function SpinPage({ loaderData, params }: Route.ComponentProps) {
 					radius="full"
 					variant="flat"
 					onPress={() => handleSpinStep("back")}
-					disabled={isPending}
+					isDisabled={isPending || isPlaying}
 				>
 					<ArrowLeft />
 				</Button>
@@ -96,7 +89,7 @@ export default function SpinPage({ loaderData, params }: Route.ComponentProps) {
 					radius="full"
 					variant="flat"
 					onPress={() => handleSpinStep("forward")}
-					disabled={isPending}
+					isDisabled={isPending || isPlaying}
 				>
 					<ArrowRight />
 				</Button>
@@ -123,4 +116,69 @@ function usePreloadSpingVideos(spin: SpinRecord[] | undefined) {
 			}
 		}
 	}, [spin]);
+}
+
+type Motion = "intro" | "forward" | "back";
+
+type SpinState = {
+	type: Motion;
+	transitionVideo: string;
+	all: Record<number, SpinRecord>;
+	spinId: number;
+	intro?: SpinRecord;
+	spin?: SpinRecord;
+};
+
+type SpinAction = { type: Motion };
+
+function reducer(state: SpinState, action: SpinAction): SpinState {
+	const len = Object.keys(state.all).length;
+	if (action.type === "forward") {
+		let spinId = (state.spinId + 1) % len;
+		let spin = state.all[spinId];
+		let transitionVideo = state?.spin?.forwardVideo;
+
+		if (state.type === "intro") {
+			spinId = 0;
+			spin = state.all[spinId];
+			transitionVideo = state.intro?.forwardVideo;
+		}
+
+		return {
+			...state,
+			spinId,
+			transitionVideo: transitionVideo || "",
+			spin,
+			type: action.type,
+		};
+	}
+
+	if (action.type === "back") {
+		const spinId = (state.spinId - 1 + len) % len;
+		const spin = state.all[spinId];
+
+		return {
+			...state,
+			spinId,
+			transitionVideo: spin?.backVideo || "",
+			spin,
+			type: action.type,
+		};
+	}
+
+	return {
+		...state,
+		transitionVideo: state.spin?.forwardVideo || "",
+		type: "intro",
+	};
+}
+
+function arrayToObject(arr: SpinRecord[]) {
+	return arr.reduce(
+		(acc, item, index) => {
+			acc[index] = item;
+			return acc;
+		},
+		{} as Record<number, SpinRecord>,
+	);
 }
